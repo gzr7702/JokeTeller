7bb0aa816ee83b640f14051487d1fbbc114a4776
Rob G <rgraziano.gzr7702@gmail.com>
2018-05-08 16:25:34 -0400

    "feat: initial commit"

---
+++ JokeProvider/src/main/java/com/example/jokeprovider/JokeProvider.java
@@ -0,0 +1,14 @@
+package com.example.jokeprovider;
+
+public class JokeProvider {
+
+    private String joke;
+
+    public JokeProvider() {
+        joke = "Why did the chicken cross the road? I'm not really sure.";
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+}
